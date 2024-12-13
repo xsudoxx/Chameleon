cd4987e9d122e9ef2dd494b52f443ef01a00d4ef
Joshua Franco <francoswim@yahoo.com>
2024-12-12 20:29:23 -0500

    adding xor_enc_dec function to chameleon.c

--- modules/xor.h
+++ modules/xor.h
@@ -3,6 +3,6 @@
 
 // Function to display help information for XOR obfuscation
 void xor_help();
-void xor_encrypt(const char* input_file, const char* output_file, const char* key); // Declaration of xor_encrypt
+void xor_enc_dec(const char* input_file, const char* output_file, const char* key); // Declaration of xor_encrypt
 
 #endif // XOR_H
